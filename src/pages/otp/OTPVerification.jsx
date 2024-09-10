10ca1f7e4e23e23a6a577b0d8641a56f9066256e
Kanchanamkl <92777840+Kanchanamkl@users.noreply.github.com>
2024-09-10 09:09:50 +0530

    removed unwanted comments

--- src/pages/otp/OTPVerification.jsx
+++ src/pages/otp/OTPVerification.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import "./OTPVerification.css"; // Import the stylesheet
+import "./OTPVerification.css"; 
 
 function OTPVerification() {
   const [otp, setOtp] = useState("");
